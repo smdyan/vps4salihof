30f53c12afb965c50eae881ce94090a8a7f896a5
smdyan <info@salihof.ru>
2024-09-06 14:25:10 +0300

    added_conf_upd.sh

---
+++ conf_upd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Start.../n"
+
+cp dokuwiki.subdomain.conf /vlm_docker/swag/config/nginx/proxy-confs
+cp depo23.subdomain.conf /vlm_docker/swag/config/nginx/proxy-confs
+
+cp default.conf /vlm_docker/dokuwiki/config/nginx/site-confs
+
+echo "Finished/n"
