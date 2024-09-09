7813da875ab173fc2b630c47a2430ee13b4da6d7
smdyan <info@salihof.ru>
2024-09-09 19:30:32 +0300

    docker profiles added for doku swag

--- conf_upd.sh
+++ conf_upd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "Start.../n"
+echo "Start..."
 
 cp dokuwiki.subdomain.conf /vlm_docker/swag/config/nginx/proxy-confs
 cp depo23.subdomain.conf /vlm_docker/swag/config/nginx/proxy-confs
 
 cp default.conf /vlm_docker/dokuwiki/config/nginx/site-confs
 
-echo "Finished/n"
+echo "Finished"
